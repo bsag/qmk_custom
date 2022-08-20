3b4d998ca62b341e335ac2d5c45b8cecbcc9e94f
bsag <butshesagirl@rousette.org.uk>
2022-08-20 13:15:58 +0100

    Altered num layer and added combos
    Moved the num/symbol layer to the left side, accessed via the outer
    right thumb key. Zero is on the outer left thumb key while in this
    layer. The symbols/shifted versions are just accessed by holding shift.
    
    Inner left thumb is now plain shift (will see how I get on with this).
    
    Holding both outer thumbs is FUN layer.
    
    Combos added back for braces and common punctuation (- _ ;).

--- mini3x5/keycodes.h
+++ mini3x5/keycodes.h
@@ -4,35 +4,21 @@
 
 #include "quantum.h"
 
-// home row mods for Platinum Hands Down
-#define HHOME_R LCTL_T(KC_R)
-#define HHOME_S LALT_T(KC_S)
-#define HHOME_N LGUI_T(KC_N)
-#define HHOME_T LSFT_T(KC_T)
+// home row mods for Colemak-DH
+#define HOME_A LCTL_T(KC_A)
+#define HOME_R LALT_T(KC_R)
+#define HOME_S LGUI_T(KC_S)
+#define HOME_T LSFT_T(KC_T)
 
-#define HHOME_A RSFT_T(KC_A)
-#define HHOME_E RGUI_T(KC_E)
-#define HHOME_C LALT_T(KC_C)
-#define HHOME_I RCTL_T(KC_I)
-
-// home row mods for QWERTY
-#define QHOME_A LCTL_T(KC_A)
-#define QHOME_R LALT_T(KC_R)
-#define QHOME_S LGUI_T(KC_S)
-#define QHOME_T LSFT_T(KC_T)
-
-#define QHOME_N RSFT_T(KC_N)
-#define QHOME_E LGUI_T(KC_E)
-#define QHOME_I LALT_T(KC_I)
-#define QHOME_O RCTL_T(KC_O)
+#define HOME_N RSFT_T(KC_N)
+#define HOME_E LGUI_T(KC_E)
+#define HOME_I LALT_T(KC_I)
+#define HOME_O RCTL_T(KC_O)
 
 // Layer keys
-#define NUM LT(_NUM, KC_ESC)
-#define FUN LT(_FUN, KC_ENT)
-#define NAV_BSPC LT(_NAV, KC_BSPC)
-
-// Hands Down only
-#define NAV_L LT(_NAV, KC_L)
+#define NUM LT(_NUM, KC_ENT)
+#define NAV LT(_NAV, KC_ESC)
+// FUN is both together
 
 // shortcuts
 #define M_UNDO G(KC_Z)
@@ -47,8 +33,6 @@
 #define WS_R A(S(KC_RIGHT))
 #define LAU LGUI(KC_SPC)
 #define CC C(KC_C)
-// to colemak layer
-#define TG_CDH TG(_CDH)
 // to hands down layer
 #define TG_PHD TG(_PHD)
 // awkward symbols to type
@@ -57,7 +41,6 @@
 
 enum layers {
     _CDH,
-	_PHD,
 	_NUM,
 	_NAV,
 	_FUN,
--- mini3x5/keymap.c
+++ mini3x5/keymap.c
@@ -10,29 +10,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Colemak Mod-DH layout
   [_CDH] = LAYOUT_split_3x5_2(
                               KC_Q, KC_W, KC_F, KC_P, KC_B,                KC_J, KC_L, KC_U,    KC_Y,   KC_QUOT,
-                              QHOME_A, QHOME_R, QHOME_S, QHOME_T, KC_G,    KC_M, QHOME_N, QHOME_E, QHOME_I, QHOME_O,
-                              KC_Z, KC_X, KC_C, KC_D, KC_V,                KC_K, KC_H, KC_COMM, KC_DOT, KC_SCLN,
-                                                 NUM, NAV_BSPC,        KC_SPC, FUN
+                              HOME_A, HOME_R, HOME_S, HOME_T, KC_G,        KC_M, HOME_N, HOME_E, HOME_I, HOME_O,
+                              KC_Z, KC_X, KC_C, KC_D, KC_V,                KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
+                                                 NAV, KC_LSFT,             KC_SPC, NUM
                               ),
 
-  // PHD = Platinum Hands Down layout
-   [_PHD] = LAYOUT_split_3x5_2(
-                              KC_J,    KC_G,    KC_H,    KC_P,    KC_V,      KC_MINS, KC_DOT,  KC_COMM, KC_QUOT, KC_DQUO,
-                              HHOME_R, HHOME_S, HHOME_N, HHOME_T, KC_B,      KC_SLSH, HHOME_A, HHOME_E, HHOME_C, HHOME_I,
-                              KC_X,    KC_F,    KC_M,    KC_D,    KC_K,      KC_BSPC, KC_U,    KC_O,    KC_W,    KC_Y,
-                                                         NUM,   NAV_L,       KC_SPC, FUN
-                              ),
 
   [_NUM] = LAYOUT_split_3x5_2(
-                              KC_NO,   PMIN,   S(KC_EQL),  KC_MINS, GBPD,   KC_GRV,  KC_7, KC_8, KC_9, KC_0,
-                              KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_NO,    KC_LBRC,  KC_4, KC_5, KC_6, KC_RBRC,
-                              KC_NO,   KC_NO,   KC_EQL,  S(KC_MINS), KC_NO,    KC_BSLS, KC_1, KC_2, KC_3, KC_SLSH,
-                                                     KC_TRNS,    KC_TRNS,   KC_SPC,  KC_TRNS
+                           KC_LBRC,  KC_7, KC_8, KC_9, KC_RBRC,        KC_NO,   GBPD,    PMIN,    KC_NO,   KC_BSPC,
+                           KC_SCLN,  KC_4, KC_5, KC_6, KC_EQL,         KC_NO,   KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,
+                           KC_GRV,   KC_1, KC_2, KC_3, KC_MINS,        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS,
+                                                 KC_DOT,  KC_0,        KC_SPC,  KC_TRNS
                               ),
 
   [_NAV] = LAYOUT_split_3x5_2(
                               SW_APP,  KC_NO,   TAB_L,  TAB_R,  KC_TAB,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
-                              KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, M_SAVE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
+                              KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, M_SAVE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,
                               M_UNDO,  M_CUT,   M_COPY, M_DUP,   M_PSTE,     WS_L,    LAU,     CC,   WS_R,    KC_DEL,
                                                          KC_TRNS, KC_TRNS,   KC_SPC, KC_TRNS
                               ),
@@ -40,10 +33,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FUN] = LAYOUT_split_3x5_2(
                               RESET,   KC_VOLD,  KC_MUTE,  KC_VOLU,   KC_CAPS,     KC_F12,  KC_F7, KC_F8, KC_F9, KC_NO,
                               KC_LCTL, KC_LALT,  KC_LGUI,  KC_LSFT,   KC_NO,       KC_F11,  KC_F4, KC_F5, KC_F6, KC_NO,
-                              TG_PHD,  KC_MPRV,  KC_MPLY,  KC_MNXT,   KC_NO,       KC_F10,  KC_F1, KC_F2, KC_F3, KC_NO,
+                              KC_NO,  KC_MPRV,  KC_MPLY,  KC_MNXT,   KC_NO,       KC_F10,  KC_F1, KC_F2, KC_F3, KC_NO,
                                                            KC_TRNS,  KC_TRNS,      KC_SPC, KC_TRNS
                               )
 };
+
+enum combo_events {
+   // punctuation combos
+   MINS_COMBO, // C+D
+   USCR_COMBO, // H+,,
+   SCLN_COMBO, // .+/
+   // braces
+   LCBR_COMBO, // F+S
+   LPRN_COMBO, // P+T
+   LBRC_COMBO, // B+G
+   RCBR_COMBO, // U+E
+   RPRN_COMBO, // L+N
+   RBRC_COMBO, // J+M
+   // Other combos...
+   COMBO_LENGTH
+ };
+ uint16_t COMBO_LEN = COMBO_LENGTH;
+// punctuation combos
+ const uint16_t PROGMEM mins_combo[] = {KC_C, KC_D, COMBO_END};
+ const uint16_t PROGMEM uscr_combo[] = {KC_H, KC_COMM, COMBO_END};
+ const uint16_t PROGMEM scln_combo[] = {KC_DOT, KC_SLSH, COMBO_END};
+ // braces - vertical combos
+ const uint16_t PROGMEM lcbr_combo[] = {KC_F, HOME_S, COMBO_END};
+ const uint16_t PROGMEM lprn_combo[] = {KC_P, HOME_T, COMBO_END};
+ const uint16_t PROGMEM lbrc_combo[] = {KC_B, KC_G, COMBO_END};
+ const uint16_t PROGMEM rbrc_combo[] = {KC_J, KC_M, COMBO_END};
+ const uint16_t PROGMEM rprn_combo[] = {KC_L, HOME_N, COMBO_END};
+ const uint16_t PROGMEM rcbr_combo[] = {KC_U, HOME_E, COMBO_END};
+
+ combo_t key_combos[] = {
+   [MINS_COMBO] = COMBO(mins_combo, KC_MINS),
+   [USCR_COMBO] = COMBO(uscr_combo, LSFT(KC_MINS)),
+   [SCLN_COMBO] = COMBO(scln_combo, KC_SCLN),
+   [LCBR_COMBO] = COMBO(lcbr_combo, KC_LCBR),
+   [LPRN_COMBO] = COMBO(lprn_combo, KC_LPRN),
+   [LBRC_COMBO] = COMBO(lbrc_combo, KC_LBRC),
+   [RCBR_COMBO] = COMBO(rcbr_combo, KC_RCBR),
+   [RPRN_COMBO] = COMBO(rprn_combo, KC_RPRN),
+   [RBRC_COMBO] = COMBO(rbrc_combo, KC_RBRC),
+ };
 
 bool sw_app_active = false;
 
@@ -58,6 +91,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   return true;
 }
 
-/* layer_state_t layer_state_set_user(layer_state_t state) { */
-/*     return update_tri_layer_state(state, _LMOD, _RMOD, _NUM); */
-/* } */
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _NUM, _NAV, _FUN);
+}
